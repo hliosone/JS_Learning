e664f0e24045f409f388b4ece0b1a3bbf5e01c6a
hliosone <sstelcher@protonmail.com>
2023-04-29 19:30:53 +0200

    Completed Practice Part 2

--- 4. Practice time - part 2/04. push, pop, unshift, shift challenge/index.js
+++ 4. Practice time - part 2/04. push, pop, unshift, shift challenge/index.js
@@ -5,3 +5,9 @@ let largeCountries = ["Tuvalu","India","USA","Indonesia","Monaco"]
 
 // Use push() & pop() and their counterparts unshift() & shift()
 // Google how to use unshift() and shift()
+largeCountries.shift()
+largeCountries.pop()
+largeCountries.push("China")
+largeCountries.unshift("Pakistan")
+
+console.log(largeCountries)
--- 4. Practice time - part 2/06. Rock papers scissors/index.js
+++ 4. Practice time - part 2/06. Rock papers scissors/index.js
@@ -1,5 +1,10 @@
 let hands = ["rock", "paper", "scissor"]
 
 // Create a function that returns a random item from the array
+function randomItem(){
+    return hands[Math.floor(Math.random() * 3)]
+}
 
-
+for(i = 0; i < 10; ++i){
+    console.log(randomItem())
+}
